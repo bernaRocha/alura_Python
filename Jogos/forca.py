536a00a140303fa1e808354b52af3f17bcfaa0e0
Bernardo <mrochabernardo@gmail.com>
2022-07-16 19:30:03 -0300

    começando o jogo da forca

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -1,10 +1,24 @@
-
-
 def jogar_forca():
 
     print("*" * 40)
     print("*********Bem vindo ao jogo da forca!*************")
     print("*" * 40)
+
+    palavra_secreta = "python"
+    
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        
+        chute = input("Qual letra? ")
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrou a letra {} na posição {}".format(letra, index))
+            index += 1
+        print("jogando........")
 
     print("Fim de jogo!")
 
