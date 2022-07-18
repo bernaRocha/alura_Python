a66f5f7ec66d7f2b0223bab88b499f2eb639f192
Bernardo <mrochabernardo@gmail.com>
2022-07-18 16:05:06 -0300

    tuplas e listas

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -1,26 +1,48 @@
+from operator import le
+
+
 def jogar_forca():
 
     print("*" * 40)
     print("*********Bem vindo ao jogo da forca!*************")
     print("*" * 40)
 
-    palavra_secreta = "python"
-    
+    palavra_secreta = "python".upper()
+    letras_acertadas = [] #lista
+#
+    for letra in palavra_secreta:
+        letras_acertadas.append("_")
+#   ou letras_acertadas = ["_" for letra in palavra_secreta]    
+#   List comprehension    
     enforcou = False
     acertou = False
+    erros = 0
+
+    print(letras_acertadas)
 
     while(not enforcou and not acertou):
         
         chute = input("Qual letra? ")
+        chute = chute.strip().upper()
+        
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                    #print("Encontrou a letra {} na posição {}".format(letra, index + 1))
+                index += 1
+        else:
+            erros += 1
+        
+        enforcou = erros == len(palavra_secreta)
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute == letra):
-                print("Encontrou a letra {} na posição {}".format(letra, index))
-            index += 1
-        print("jogando........")
+    if(acertou):
+        print("Você ganhou! parabéns!")
 
-    print("Fim de jogo!")
+    print("Fim de jogo! Tente novamente!")
 
 if(__name__== "__main__"):
     jogar_forca()
