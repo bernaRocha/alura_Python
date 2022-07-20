f4018e42123ae26c3892c69c509f11910f37bd56
Bernardo <mrochabernardo@gmail.com>
2022-07-20 12:52:06 -0300

    class Data

--- object_oriented/conta.py
+++ object_oriented/conta.py
@@ -5,22 +5,26 @@ class Conta:
     
     def __init__(self, numero, titular, saldo, limite): #atributos #função construtora da classe, recebendo uma referência do próprio objeto como argumento
         print("Construindo o objeto .. {}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
-    
+        self.__numero = numero   # __ torna o atributo privado
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+ ###### métodos  
+ 
     def extrato(self):
         print("Saldo de {} do titular {}".format(self.saldo, self.titular))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def sacar(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
 
 #>>> from conta import Conta
 #>>> Conta()
 #<conta.Conta object at 0x7f181716bc10>
 # valores dos atributos da classe: numero, titular, saldo e limite.
 #>>> conta.extrato() / dentro do objeto conta acessar o extrato()
+
+conta = Conta(34567, "Bernardo", 131.0, 2000.0)
---
+++ object_oriented/data.py
@@ -0,0 +1,8 @@
+class Data:
+    def __init__(self,data):
+        self.data = data
+        
+
+from datas import Data
+d = Data(21,11,2007)
+d.formatada()
