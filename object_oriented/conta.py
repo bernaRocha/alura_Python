582d3690e39538eda43efa3569b046f2707f8a0c
Bernardo <mrochabernardo@gmail.com>
2022-07-20 10:33:59 -0300

    criando atributos

--- object_oriented/conta.py
+++ object_oriented/conta.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python
 
-# receita
+# uma class é como uma receita
 class Conta: 
     
-    def __init__(self, numero, titular, saldo, limite):
-        print("Construindo objeto .. {}".format(self))
+    def __init__(self, numero, titular, saldo, limite): #atributos #função construtora da classe, recebendo uma referência do próprio objeto como argumento
+        print("Construindo o objeto .. {}".format(self))
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+    
+    def extrato(self):
+        print("Saldo de {} do titular {}".format(self.saldo, self.titular))
 
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def sacar(self, valor):
+        self.saldo -= valor
 
 #>>> from conta import Conta
 #>>> Conta()
 #<conta.Conta object at 0x7f181716bc10>
+# valores dos atributos da classe: numero, titular, saldo e limite.
+#>>> conta.extrato() / dentro do objeto conta acessar o extrato()
