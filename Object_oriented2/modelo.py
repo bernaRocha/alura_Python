df1e62885116e4fe817e56d5d21fbcb5cd88be5e
Bernardo <mrochabernardo@gmail.com>
2022-08-14 16:56:55 -0300

    relembrando classes e objetos

---
+++ Object_oriented2/modelo.py
@@ -0,0 +1,43 @@
+class Filme:
+    def __init__(self, nome, ano, duracao):
+        self.__nome = nome.title()
+        self.ano = ano
+        self.duracao = duracao
+        self.__likes = 0 # o obj começa com ) likes
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def adicionar_like(self):
+        self.likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+class Serie:
+    def __init__(self, nome, ano, temporadas):
+        self.__nome = nome.title()
+        self.ano = ano
+        self.temporadas = temporadas
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def adicionar_like(self):
+        self.likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
