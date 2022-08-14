da85c3c6401011f846e564239f564ecdcfaacb0c
Bernardo <mrochabernardo@gmail.com>
2022-08-14 17:36:26 -0300

    Refatoração das classes

--- Object_oriented2/modelo.py
+++ Object_oriented2/modelo.py
@@ -1,43 +1,44 @@
-class Filme:
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0 
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def adicionar_like(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+class Filme(Programa):  # Filme herda de Programa
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano) # chama da classe Mãe para evitar repetição de código
         self.duracao = duracao
-        self.__likes = 0 # o obj começa com ) likes
+     
 
-    @property
-    def likes(self):
-        return self.__likes
-
-    def adicionar_like(self):
-        self.likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__likes = 0
 
-    @property
-    def likes(self):
-        return self.__likes
 
-    def adicionar_like(self):
-        self.likes += 1
 
-    @property
-    def nome(self):
-        return self.__nome
 
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
+
+godzilla = Filme('godzilla - rei dos mosntros', 2018, 127)
+godzilla.adicionar_like()
+print(f'Nome: {godzilla.nome} - Ano {godzilla.nome} - Duração {godzilla.duracao} - Likes {godzilla.likes}')
+
+dark = Serie('dark', 2019, 3)
+dark.adicionar_like()
+dark.adicionar_like()
+print(f'Nome: {dark.nome} - Ano: {dark.ano} - Temporadas: {dark.temporadas} - Likes: {dark.likes}')
