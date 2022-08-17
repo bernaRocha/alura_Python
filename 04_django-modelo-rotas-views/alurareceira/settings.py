28c2d13ad757db2180f1e6264d57dfd55afee07f
Bernardo <mrochabernardo@gmail.com>
2022-08-17 10:57:55 -0300

    erro no collectstatic

--- 04_django-modelo-rotas-views/alurareceira/settings.py
+++ 04_django-modelo-rotas-views/alurareceira/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'receitas',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'alurareceira.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'receitas/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/templates')
+STATIC_URL = 'alurareceita/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'alurareceita/static')
+]
---
+++ 04_django-modelo-rotas-views/receitas/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ReceitasConfig(AppConfig):
+    name = 'receitas' # nome para registrar a aplaicação
