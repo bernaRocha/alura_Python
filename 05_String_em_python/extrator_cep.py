8305535344e662e8c3838f57990e5b40cafd19aa
Bernardo <mrochabernardo@gmail.com>
2022-08-20 15:09:13 -0300

    simplificando o regex

--- 05_String_em_python/extrator_cep.py
+++ 05_String_em_python/extrator_cep.py
@@ -1,15 +1,18 @@
 import re # Expressões regulares || RegEx
 
-padrao = re.compile()
+padrao = re.compile("[0-9]{5}[-]{0,1}[0-9]{3}")
 
-endereco = "Rua das Flores, apartamento 101, \
-            Bairro Inexistente, Rio de Fevereiro, SJ, \
-            23434-590"
+endereco = "Rua das Flores, apartamento 101, "\
+            "Bairro Inexistente, Rio de Fevereiro, SJ, "\
+            "23434-590"
 
 # [-]? pode ter OU não o hífen
 
-busca = padrao.search(endereco)
+busca = padrao.search(endereco) # retorna True ou False
 
 if busca:
-    cep = busca.group()
+    cep = busca.group() # retorna a string encontrada no padrão
     print(cep)
+
+#### Como simplificar o 
+###   "[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]"
