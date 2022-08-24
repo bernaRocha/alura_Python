17ea1c7865125773443b80c15932312043bf50e7
Bernardo <mrochabernardo@gmail.com>
2022-08-23 21:23:47 -0300

    Refatorando

--- 06_python_tdd/codigo/bytebank.py
+++ 06_python_tdd/codigo/bytebank.py
@@ -25,15 +25,15 @@ class Funcionario:
         nome_quebrado = nome_completo.split(' ')
         return nome_quebrado[-1]
 
-    def decrescimo_salario(self):
+    def _eh_socio(self):
         sobrenomes = ['Bragança', 'Windsor', 'Yamato']
-        if self._salario >= 100000 and (self.sobrenome() in sobrenomes):
+        return (self._salario >= 100000) and (self.sobrenome() in sobrenomes)
+       
+    def decrescimo_salario(self):
+        if self._eh_socio():
             decrescimo = self._salario * 0.1
             self._salario = self._salario - decrescimo
         
-
-
-
     def calcular_bonus(self):
         valor = self._salario * 0.1
         if valor > 1000:
@@ -43,3 +43,4 @@ class Funcionario:
    
     def __str__(self):
         return f'Funcionário({self._nome}, {self._data_nascimento}, {self._salario})'
+        
