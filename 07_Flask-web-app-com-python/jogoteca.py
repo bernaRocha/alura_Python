0b5ee3df562004e5947692a99eb87e3dbd99a322
Bernardo <mrochabernardo@gmail.com>
2022-08-25 18:58:45 -0300

    rename

--- 07_Flask-web-app-com-python/jogoteca.py
+++ 07_Flask-web-app-com-python/jogoteca.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
 app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    lista_jogos = ['Tetris', 'Scorn', 'Skyrin', 'Stray']
+    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+    jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+    jogo3 = Jogo('Scorn', 'Terror', 'PC')
+    jogo4 = Jogo('Stray', 'Adventure', 'PC')
+    
+    lista_jogos = [jogo1, jogo2, jogo3, jogo4]
+    
     return render_template('lista.html', titulo='Jogos', jogos=lista_jogos )
 
 app.run()
