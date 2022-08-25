e91aaeb6b9f50aec8d0f347258dbb5616e40bfb3
Bernardo <mrochabernardo@gmail.com>
2022-08-25 20:39:50 -0300

    mais uma rota

--- 07_Flask-web-app-com-python/jogoteca.py
+++ 07_Flask-web-app-com-python/jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -6,23 +6,34 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+jogo3 = Jogo('Scorn', 'Terror', 'PC')
+jogo4 = Jogo('Stray', 'Adventure', 'PC')
+jogo5 = Jogo('Mortal Kombat', 'Gore', 'PS1')
+    
+lista_jogos = [jogo1, jogo2, jogo3, jogo4, jogo5]
 
 app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
-    jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
-    jogo3 = Jogo('Scorn', 'Terror', 'PC')
-    jogo4 = Jogo('Stray', 'Adventure', 'PC')
-    jogo5 = Jogo('Mortal Kombat', 'Gore', 'PS1')
     
-    lista_jogos = [jogo1, jogo2, jogo3, jogo4, jogo5]
     
     return render_template('lista.html', titulo='Jogos', jogos=lista_jogos )
 
 @app.route('/novo')
 def novo():
     return render_template('novo.html', titulo='Novo jogo')
+
+@app.route('/criar')
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista_jogos.append(jogo)
+
+    return render_template('lista.html', titulo='Jogos', jogos=lista_jogos)
 
 app.run()
